a09ffec30d736e1143a551e139c37c84741973cc
Aditya Amit Kinjawadekar <programmeraditya36@gmail.com>
2024-09-11 23:54:31 +0530

    started

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,5 @@
-
-            import React from 'react';
-    import ReactDOM from 'react-dom';
-    import App from './App';
-    import { BrowserRouter } from 'react-router-dom';
-    import './index.css';
-    ReactDOM.render(
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>,
-        document.getElementById('root')
-    );
-            
+import { createRoot } from "react-dom/client";
+import App from "./App";
+const container = document.getElementById("root");
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(<App />);
